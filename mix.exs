a6c6228fec1b94ca88d251d2683007e6bc3da14c
LJZN <forbitcoincash@gmail.com>
2024-02-20 08:13:02 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCRC.Mixfile do
   def project do
     [
       app: :ex_crc,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.5",
       description: description(),
       package: package(),
